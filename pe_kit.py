0cf1417fe0bc851af561ef888a5a424e6c52b708
Geoff Williams <geoff.williams@puppetlabs.com>
2016-03-21 23:09:15 +1100

    added warnings when not launched from terminal or image not selected

--- pe_kit.py
+++ pe_kit.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
 from docker import Client
 from docker.utils import kwargs_from_env
 import webbrowser
@@ -15,6 +16,7 @@ from utils import Utils
 class MainScreen(GridLayout):
   DOCKER_CONTAINER="pe_kit__"
   PE_HOSTNAME="pe-puppet.localdomain"
+  DOCKER_IMAGE_PATTERN="geoffwilliams/pe_master_public_lowmem"
   cli = None
   docker_url = None
   pe_url = None
@@ -58,39 +60,45 @@ class MainScreen(GridLayout):
 
   def start_pe(self):
     if not self.container_running:
-      self.container = self.cli.create_container(
-        image=self.get_selected_image(),
-        name=self.DOCKER_CONTAINER,
-        hostname=self.PE_HOSTNAME,
-        detach=True,
-        volumes = [
-            "/sys/fs/cgroup",
-        ],
-      )
-      resp = self.cli.start(
-        container=self.container.get('Id'), 
-        privileged=True,
-        publish_all_ports=True,
-      )
-      self.log(resp)
-      self.container_running = True
+      if self.get_selected_image().startswith(self.DOCKER_IMAGE_PATTERN):
+        self.container = self.cli.create_container(
+          image=self.get_selected_image(),
+          name=self.DOCKER_CONTAINER,
+          hostname=self.PE_HOSTNAME,
+          detach=True,
+          volumes = [
+              "/sys/fs/cgroup",
+          ],
+        )
+        resp = self.cli.start(
+          container=self.container.get('Id'), 
+          privileged=True,
+          publish_all_ports=True,
+        )
+        self.log(resp)
+        self.container_running = True
 
-      # inspect the container and get the port mapping
-      container_info = self.cli.inspect_container(self.container.get("Id"))
-      pp = pprint.PrettyPrinter()
-      pp.pprint(container_info)
-      self.log(pp.pformat(container_info))
+        # inspect the container and get the port mapping
+        container_info = self.cli.inspect_container(self.container.get("Id"))
+        pp = pprint.PrettyPrinter()
+        pp.pprint(container_info)
+        self.log(pp.pformat(container_info))
 
-      self.pe_console_port = container_info["NetworkSettings"]["Ports"]["443/tcp"][0]["HostPort"]
+        self.pe_console_port = container_info["NetworkSettings"]["Ports"]["443/tcp"][0]["HostPort"]
 
-      
-      # update the URL to browse to
-      parsed = urlparse(self.docker_url)
-      self.pe_url = parsed._replace(
-        netloc="{}:{}".format(parsed.hostname, self.pe_console_port)
-      ).geturl()
+        
+        # update the URL to browse to
+        parsed = urlparse(self.docker_url)
+        self.pe_url = parsed._replace(
+          netloc="{}:{}".format(parsed.hostname, self.pe_console_port)
+        ).geturl()
+      else:
+        self.error("Please select an image from the list first")
+    return self.container_running
 
-
+  def error(self, message):
+    popup = Popup(title='Error!', content=Label(text=message), size_hint=(0.8,0.5))
+    popup.open()
 
   def stop_pe(self):
     if self.container_running:
@@ -101,52 +109,54 @@ class MainScreen(GridLayout):
     return self.docker_image_button.text
 
   def pe_terminal(self, instance):
-    self.start_pe()
-    Utils.docker_terminal("docker exec -ti {name} bash".format(
-      name=self.DOCKER_CONTAINER,
-    ))
-    self.log("TERM clicked")
+    if self.start_pe():
+      Utils.docker_terminal("docker exec -ti {name} bash".format(
+        name=self.DOCKER_CONTAINER,
+      ))
 
   def pe_console(self, instance):
-    self.start_pe()
-  
-    self.log("CONSOLE clicked")
-    webbrowser.open_new(self.pe_url)
+    if self.start_pe():
+      webbrowser.open_new(self.pe_url)
 
 
   def docker_init(self):
     #  boot2docker specific hacks in use - see:  http://docker-py.readthedocs.org/en/latest/boot2docker/
     self.log("***DOCKER INIT***")
     kwargs = kwargs_from_env()
-    kwargs['tls'].assert_hostname = False
 
-    # save the boot2docker IP for use when we open browser windows
-    self.docker_url = kwargs['base_url'] 
+    if 'tls' not in kwargs:
+      self.error("Couldn't find docker!  Please run from Docker Quickstart Terminal!")
+    else:  
+      kwargs['tls'].assert_hostname = False
 
-    self.cli = Client(**kwargs)
+      # save the boot2docker IP for use when we open browser windows
+      self.docker_url = kwargs['base_url'] 
+
+      self.cli = Client(**kwargs)
     self.container_running = False
 
   def update_docker_images(self):
     if self.cli is None:
       self.docker_init()
-  
-    dropdown = DropDown()
-    docker_images = self.cli.images()
-    pp = pprint.PrettyPrinter()
-    pp.pprint(docker_images)
-    for docker_image in docker_images:
-      image_name = docker_image["RepoTags"][0]
-      self.log("found image " + image_name)
-      if image_name.startswith("geoffwilliams/pe_master_public_lowmem"):
-        btn = Button(text=image_name, size_hint_y=None, height=44)
-        btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-        dropdown.add_widget(btn)
+ 
+    if self.cli is not None: 
+      dropdown = DropDown()
+      docker_images = self.cli.images()
+      pp = pprint.PrettyPrinter()
+      pp.pprint(docker_images)
+      for docker_image in docker_images:
+        image_name = docker_image["RepoTags"][0]
+        self.log("found image " + image_name)
+        if image_name.startswith(self.DOCKER_IMAGE_PATTERN):
+          btn = Button(text=image_name, size_hint_y=None, height=44)
+          btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+          dropdown.add_widget(btn)
 
 
-    #self.docker_image_button = Button(text='Available images', size_hint=(1, 1))
-    self.docker_image_button.bind(on_release=dropdown.open)
-    #self.add_widget(self.docker_image_button)
-    dropdown.bind(on_select=lambda instance, x: setattr(self.docker_image_button, 'text', x))  
+      #self.docker_image_button = Button(text='Available images', size_hint=(1, 1))
+      self.docker_image_button.bind(on_release=dropdown.open)
+      #self.add_widget(self.docker_image_button)
+      dropdown.bind(on_select=lambda instance, x: setattr(self.docker_image_button, 'text', x))  
 
 
 class PeKitApp(App):
