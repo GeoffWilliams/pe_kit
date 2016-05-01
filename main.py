007a653bc6ec2f97d657ad79da7020dbb98ee44d
Geoff Williams <geoff.williams@puppetlabs.com>
2016-05-01 21:12:46 +1000

    version

--- main.py
+++ main.py
@@ -1156,7 +1156,7 @@ class PeKitApp(App):
     """
     logger = logging.getLogger(__name__)
     settings = Settings()
-    __version__ = "v0.2.0"
+    __version__ = "v0.2.1"
     
     def check_update(self):
         """check for new release of the app"""
